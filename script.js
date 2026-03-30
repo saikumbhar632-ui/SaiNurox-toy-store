63faf977bae84728ec7e0ae6dcea80cb77282e28
saikumbhar632-ui <saikumbhar632@gmail.com>
2026-03-30 11:30:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// ORDER MESSAGE
+function placeOrder() {
+    alert("✅ Your Order Placed Successfully!");
+}
+
+// RATING
+function rate(star) {
+    document.getElementById("ratingText").innerHTML =
+        "You rated " + star + " ⭐";
+}
